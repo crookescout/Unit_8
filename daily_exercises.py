e72e23e854b2ca31cb2e8496da3b19309376919c
scout <scout.crooke@ssfs.org>
2019-11-15 10:08:42 -0500

    worked on daily exercises

---
+++ daily_exercises.py
@@ -0,0 +1,11 @@
+# Scout Crooke, 11/15/19, this function works on daily exerceises for Unit 8
+
+from tkinter import *
+
+root = Tk()
+
+# 1
+
+hello_label = Label(root, text="Hello, World")
+hello_label.grid(row=1, column=1)
+
