6268e630ac98e774a29e5d0d165e4559422bde72
scout <scout.crooke@ssfs.org>
2019-11-15 10:13:47 -0500

    worked on daily exercises for unit 8

--- daily_exercises.py
+++ daily_exercises.py
@@ -5,7 +5,13 @@ from tkinter import *
 root = Tk()
 
 # 1
+#
+# hello_label = Label(root, text="Hello, World")
+# hello_label.grid(row=1, column=1)
 
-hello_label = Label(root, text="Hello, World")
-hello_label.grid(row=1, column=1)
+#2
 
+hello_button = Button(root, text="Roll Die")
+hello_button.grid(row=3, column=1)
+
+root.mainloop()
